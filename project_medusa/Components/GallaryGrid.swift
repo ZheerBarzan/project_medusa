67c479a06b006562aab565b6a621ca4df84f5a71
ZheerBarzan <zheerbarzan85@outlook.com>
2025-02-15 10:37:59 +0300

    prototyping the library page

--- project_medusa/Components/GallaryGrid.swift
+++ project_medusa/Components/GallaryGrid.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct GallaryGrid: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            List{
+                Text("hello")
+                    .shadow(radius: 10)
+                    .swipeActions(edge: .leading){
+                        Button(role: .destructive){
+                            
+                        } label: {
+                            Image(systemName: "trash")
+                        }
+                        
+                        Button(){
+                            
+                        }label: {
+                            Image(systemName: "gear")
+                        }
+                    }
+                    .swipeActions(edge: .trailing) {
+                        Button(){
+                            
+                        } label: {
+                            Image(systemName: "share")
+                        }
+                    }
+            }
+        
     }
 }
 
