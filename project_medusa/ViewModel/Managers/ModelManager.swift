1a9ab03d6c2f03440ece3385e59651df5bdd0a53
Zheer Barzan <zheerbarzan85@outlook.com>
2025-04-22 16:08:46 +0300

    i will finish it at home i cant think right now

--- project_medusa/ViewModel/Managers/ModelManager.swift
+++ project_medusa/ViewModel/Managers/ModelManager.swift
@@ -18,5 +18,26 @@ class ModelManager{
     
     func getAllModels() async -> [ModelObjectItem]{
         
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else{
+            return []
+        }
+        
+        do {
+            let folderURLs = try FileManager.default.contentsOfDirectory(
+                at: documentsDirectory,
+                includingPropertiesForKeys: [.creationDateKey],
+                options: []
+            ).filter{ $0.hasDirectoryPath}
+                .sorted(by: {
+                    let date1 = try? $0.resourceValues(forKeys: [.creationDateKey]).creationDate ?? Date()
+                    let date2 = try? $1.resourceValues(forKeys: [.creationDateKey]).creationDate ?? Date()
+                    
+                    return date1 ?? Date() > date2 ?? Date()
+
+                })
+            
+            
+        }
+        
     }
 }
